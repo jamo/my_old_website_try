0a484bd1529cc45d85a9ecb69696f322a5f3723e
Jarmo Isotalo <jamo@isotalo.fi>
2012-09-27 01:09:00 +0300

    init

---
+++ js/blink.js
@@ -0,0 +1,45 @@
+var blink_closure = (function () {
+  var state = "on";
+  return function() {
+    if(state == "on"){
+      document.getElementById("cursor").style.visibility="hidden";
+      state = "off";
+    } else {
+      document.getElementById("cursor").style.visibility="visible";
+      state = "on";
+    }
+    return true;
+  };
+})();
+
+function six(){
+  $("#6").show();
+  setInterval(blink_closure, 1000);
+}
+
+function five(){
+  $("#5").show().jTypeWriter({duration:0.5});;
+  setTimeout(six, 1000);
+}
+
+function four(){
+  $("#4").show();
+  setTimeout(five, 1000);
+}
+
+function three(){
+  $("#3").show();
+  setTimeout(four, 1500);
+}
+
+function two(){
+  $("#2").show().jTypeWriter({duration:0.7});
+  setTimeout(three, 1000);
+}
+
+function one(){
+  $("#1").show();
+  setTimeout(two, 1000);
+}
+
+$(document).ready(one);
